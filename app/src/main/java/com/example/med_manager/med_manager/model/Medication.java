2993650f2a20da2b8cbc03adc390938650e7329e
Salabson <salabson4@yahoo.co.uk>
2018-04-04 15:11:53 +0100

    create and implement medication model

---
+++ app/src/main/java/com/example/med_manager/med_manager/model/Medication.java
@@ -0,0 +1,47 @@
+package com.example.med_manager.med_manager.model;
+
+/**
+ * Created by salabs on 04/04/2018.
+ */
+
+public class Medication {
+    private int id;
+    private String Name;
+    private  String Description;
+    private int Frequency;
+    private String StartDate;
+    private String EndDate;
+
+    public Medication(int id, String name, String description, int frequency, String startDate, String endDate) {
+        this.id = id;
+        Name = name;
+        Description = description;
+        Frequency = frequency;
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public int getFrequency() {
+        return Frequency;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public String getEndDate() {
+        return EndDate;
+    }
+}
