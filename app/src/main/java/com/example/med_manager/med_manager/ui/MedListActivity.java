25089c7a2ae1cc209a392c5129e651c87c01de90
Salabson <salabson4@yahoo.co.uk>
2018-04-04 15:05:36 +0100

    implement medlistadapter basic code

---
+++ app/src/main/java/com/example/med_manager/med_manager/ui/MedListActivity.java
@@ -0,0 +1,8 @@
+package com.example.med_manager.med_manager.ui;
+
+/**
+ * Created by salabs on 04/04/2018.
+ */
+
+public class MedListActivity {
+}
--- app/src/main/java/com/example/med_manager/med_manager/ui/MedListAdapter.java
+++ app/src/main/java/com/example/med_manager/med_manager/ui/MedListAdapter.java
@@ -1,8 +1,47 @@
 package com.example.med_manager.med_manager.ui;
 
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.med_manager.R;
+
 /**
  * Created by salabs on 03/04/2018.
  */
 
-public class MedListAdapter {
+public class MedListAdapter extends RecyclerView.Adapter<MedListAdapter.MedViewHolder> {
+
+    @Override
+    public MedListAdapter.MedViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MedListAdapter.MedViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    /**
+     * PlantViewHolder class for the recycler view item
+     */
+    class MedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView medName;
+        TextView medDesc;
+
+        public MedViewHolder(View itemView) {
+            super(itemView);
+            medName = (TextView) itemView.findViewById(R.id.med_name);
+            medDesc = (TextView) itemView.findViewById(R.id.med_decs);
+        }
+    }
 }
